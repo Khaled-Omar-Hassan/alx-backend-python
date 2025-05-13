97bc3fa406c0583feb120e3d039ba441705754eb
Khaled Omar <khaledomar3112@gmail.com>
2025-05-13 16:20:54 +0300

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,15 @@
+import seed
 
+
+
+def stream_users():
+  db = seed.connect_db()
+  seed.connect_to_prodev(db)
+  seed.create_database(db)
+  seed.create_table(db)
+  seed.insert_data(db)
+
+  for row in db.cursor().execute("SELECT * FROM user_data;"):
+    yield(row)
+
+  
