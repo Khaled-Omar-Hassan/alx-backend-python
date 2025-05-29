0893fd2bc487961661d9048e4b6eb340a30cc776
Khaled Omar <khaledomar3112@gmail.com>
2025-05-29 20:42:44 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,23 +1,77 @@
 #!/usr/bin/env python3
-"""Test suite for GithubOrgClient class."""
+"""Generic utilities for github org client.
+"""
+import requests
+from functools import wraps
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
 
-import unittest
-from unittest.mock import patch
-from parameterized import parameterized
-from client import GithubOrgClient
+__all__ = [
+    "access_nested_map",
+    "get_json",
+    "memoize",
+]
 
 
-class TestGithubOrgClient(unittest.TestCase):
-    """Test suite for GithubOrgClient class."""
-    @parameterized.expand([("google",), ("abc",)])
-    @patch("utils.requests.get")
-    def test_org(self, org, request_mock):
-        """Test that org method returns the correct org."""
-        request_mock.return_value.json.return_value = 'test'
-        obj = GithubOrgClient(org)
-        self.assertEqual(obj.org, 'test')
-        request_mock.assert_called_once_with(obj.ORG_URL.format(org=org))
+
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
+    """Access nested map with key path.
+    Parameters
+    ----------
+    nested_map: Mapping
+        A nested map
+    path: Sequence
+        a sequence of key representing a path to the value
+    Example
+    -------
+    >>> nested_map = {"a": {"b": {"c": 1}}}
+    >>> access_nested_map(nested_map, ["a", "b", "c"])
+    1
+    """
+    for key in path:
+        if not isinstance(nested_map, Mapping):
+            raise KeyError(key)
+        nested_map = nested_map[key]
+
+    return nested_map
+
+def add(a, b):
+    return a+b
+
+def get_json(url: str) -> Dict:
+    """Get JSON from remote URL.
+    """
+    response = requests.get(url)
+    return response.json()
 
 
-if __name__ == "__main__":
-    unittest.main()
+def memoize(fn: Callable) -> Callable:
+    """Decorator to memoize a method.
+    Example
+    -------
+    class MyClass:
+        @memoize
+        def a_method(self):
+            print("a_method called")
+            return 42
+    >>> my_object = MyClass()
+    >>> my_object.a_method
+    a_method called
+    42
+    >>> my_object.a_method
+    42
+    """
+    attr_name = "_{}".format(fn.__name__)
+    @wraps(fn)
+    def memoized(self):
+        """"memoized wraps"""
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return property(memoized)
