6c171641ec440b92685fdd76ee788683af4f6cf5
Khaled Omar <khaledomar3112@gmail.com>
2025-06-08 03:29:39 +0300

    .

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,2 +1,14 @@
 from rest_framework import permissions
 from rest_framework.permissions import BasePermission, IsAuthenticated
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+
+        # Instance must have an attribute named `owner`.
+        return obj.owner == request.user
--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
+from .auth import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet,
@@ -14,4 +15,6 @@ convo_router.register(r'messages', MessageViewSet,
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(convo_router.urls)),
+    path('token/', TokenObtainPairView, name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView, name='token_refresh'),
 ]
