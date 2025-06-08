31d121d54976f452b29879dfdf3b1f77adeb3ad3
Khaled Omar <khaledomar3112@gmail.com>
2025-06-08 08:40:04 +0300

    Update middleware.py

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from django.http import HttpResponse
 
 logging.basicConfig(
     filename='requests.log',
@@ -17,6 +18,26 @@ class RequestLoggingMiddleware:
         # Code to be executed for each request before
         # the view (and later middleware) are called.
         logging.info(f"User: {request.user.id}")
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        current_hour = datetime.now().hour
+        if current_hour < 9 or current_hour > 17:
+            return HttpResponse("Access restricted to business hours (9 AM - 5 PM)", status=403)
+
         response = self.get_response(request)
 
         # Code to be executed for each request/response after
