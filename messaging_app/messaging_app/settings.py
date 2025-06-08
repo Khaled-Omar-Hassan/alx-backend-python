65261218fc3abe7c5457b98f94810c40bdac2cdc
Khaled Omar <khaledomar3112@gmail.com>
2025-06-08 08:09:13 +0300

    pag

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -77,6 +77,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
 
 WSGI_APPLICATION = "messaging_app.wsgi.application"
