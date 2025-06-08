2022f82e74e415ff251c7f08dc9fc7490e846099
Khaled Omar <khaledomar3112@gmail.com>
2025-06-08 03:33:12 +0300

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -2,13 +2,16 @@ from rest_framework import permissions
 from rest_framework.permissions import BasePermission, IsAuthenticated
 
 
-class IsOwner(permissions.BasePermission):
+class IsParticipantOfConversation(BasePermission):
     """
     Object-level permission to only allow owners of an object to edit it.
     Assumes the model instance has an `owner` attribute.
     """
 
     def has_object_permission(self, request, view, obj):
-
+        if not request.user or not request.user.is_authenticated:
+            # If the user is authenticated, check if they are a participant
+            return False
         # Instance must have an attribute named `owner`.
-        return obj.owner == request.user
+        conversation = getattr(obj, 'conversation', obj)
+        return request.user in conversation.participants.all()
